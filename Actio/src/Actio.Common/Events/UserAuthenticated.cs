c6ee4f3b5e6de880b0c07c89ac2c554c69bb00d9
Frederic Torres <fredericaltorres@gmail.com>
2019-05-26 22:37:26 +0000

    update...

---
+++ Actio/src/Actio.Common/Events/UserAuthenticated.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actio.Common.Events
+{
+    public class UserAuthenticated : IEvent
+    {
+        public string Email { get; }
+
+        protected UserAuthenticated() // For serializer
+        {
+        }
+
+        public UserAuthenticated(string email)
+        {
+            this.Email = email;
+        }
+    }
+}
