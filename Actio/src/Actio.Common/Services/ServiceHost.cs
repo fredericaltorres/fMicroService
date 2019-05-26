c1816d513d7819bdd3598d40510b9ed8f076a092
Frederic Torres <fredericaltorres@gmail.com>
2019-05-26 22:50:13 +0000

    update...

---
+++ Actio/src/Actio.Common/Services/ServiceHost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actio.Common.Services
+{
+    public class ServiceHost : IServiceHost
+    {
+        public void Run()
+        {
+        }
+    }
+}
