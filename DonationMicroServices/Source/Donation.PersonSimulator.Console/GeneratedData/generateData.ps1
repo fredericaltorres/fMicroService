c4080a1e324227c1ae89a88b4b0326e8af9f7e95
ftorres <ftorres@emoneyadvisor.com>
2019-06-02 01:19:04 -0400

    Create powershell script to generate data with each file containing 50 000 row for a total of 500 000 rwos

---
+++ DonationMicroServices/Source/Donation.PersonSimulator.Console/GeneratedData/generateData.ps1
@@ -0,0 +1,40 @@
+
+$MAX_DONATION_PER_HTTP_REQUEST = 5000
+
+function generateOneSetOf5000Donation() {
+
+    $httpResponse = curl "https://api.mockaroo.com/api/223f6d70?count=$($MAX_DONATION_PER_HTTP_REQUEST)&key=3adfa220"
+    $content = $httpResponse.Content
+    $content = $content.replace("[{", "{").Replace("}]", "}")
+    return $content
+}
+
+function generateDonationIndex($index) {
+
+    Write-Host "Generate data for donation $index"
+
+    $JsonBlock = New-Object Collections.Generic.List[String]
+
+    for($i=0; $i -lt 10; $i++) {
+
+        Write-Host "Downloading data for donation $index / $i"
+
+        $JsonBlock.Add((generateOneSetOf5000Donation))
+    }
+
+    $FinalJson = $JsonBlock -join " `r`n,`r`n"
+    $FinalJson = "[`r`n $FinalJson `r`n]"
+
+    $FinalJson | Set-Content "./donation$($index).json"
+
+}
+
+
+cls
+
+"Donation Data Generation"
+
+for($index=0; $index -lt 10; $index++) {
+
+    generateDonationIndex $index
+}
