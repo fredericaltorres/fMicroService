b59def11b8fab01bc80a967a607dace1ebc87bd3
Frederic Torres <fredericaltorres@gmail.com>
2019-08-16 04:20:48 +0000

    Add Jenkins pipeline

--- build.ps1
+++ build.ps1
@@ -1,12 +1,18 @@
-Write-Output "building from powershell"
+Write-Output "************************"
+Write-Output "Building from powershell"
+Write-Output "************************"
 
 Set-Location ".\DonationMicroServices\Source"
 
+function restoreFile([string]$file, [string]$sourceFile) {
+    if(-not (Test-Path $file)) {
+        Write-Output "Restore file $file"
+        copy $sourceFile $file
+    }
+}
+
 $sourceFile = "C:\DVT\microservices\fMicroService\DonationMicroServices\Source\Donation.PersonSimulator.Console\appsettings.json"
 $file = ".\Donation.PersonSimulator.Console\appsettings.json"
-if(-not (Test-Path $file)) {
-    Write-Output "Restore file $file"
-    copy $sourceFile $file
-}
+restoreFile $file $sourceFile
 
 .\DeploymentUtilityMaster.ps1 -a build -app Donation.PersonSimulator.Console
