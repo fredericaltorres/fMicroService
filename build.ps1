bf6ed2c5dd51ab9d527ee83d4bb01bf779abc4d3
Frederic Torres <fredericaltorres@gmail.com>
2019-08-20 02:00:58 +0000

    Update jenkins build

--- build.ps1
+++ build.ps1
@@ -10,9 +10,19 @@ function restoreFile([string]$file, [string]$sourceFile) {
         copy $sourceFile $file
     }
 }
-
+# Restorer appsettings.json file for Donation.PersonSimulator.Console
 $sourceFile = "C:\DVT\microservices\fMicroService\DonationMicroServices\Source\Donation.PersonSimulator.Console\appsettings.json"
 $file = ".\Donation.PersonSimulator.Console\appsettings.json"
+restoreFile $file $sourceFile
+
+# Restorer appsettings.json file for Donation.RestApi.Entrance
+$sourceFile = "C:\DVT\microservices\fMicroService\DonationMicroServices\Source\Donation.RestApi.Entrance\appsettings.json"
+$file = ".\Donation.RestApi.Entrance\appsettings.json"
+restoreFile $file $sourceFile
+
+# Restorer appsettings.json file for Donation.RestApi.Entrance
+$sourceFile = "C:\DVT\microservices\fMicroService\DonationMicroServices\Source\Donation.QueueProcessor.Console\appsettings.json"
+$file = ".\Donation.QueueProcessor.Console\appsettings.json"
 restoreFile $file $sourceFile
 
 .\DeploymentUtilityMaster.ps1 -a build -app Donation.PersonSimulator.Console
